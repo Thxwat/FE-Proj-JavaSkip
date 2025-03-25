02aab0195973c1d3750f45a0eb275b65e3a4ef8d
Thxwat <tanawatmiracle@gmail.com>
2025-03-25 18:05:00 +0700

    OOQ5

--- src/components/CGCatalog.tsx
+++ src/components/CGCatalog.tsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
 import Card from "./Card";
+import { cgJson } from "../../interface";
+import { cgItem } from "../../interface";
 
-export default function CGCatalog({cgJson}:{cgJson:Object}) {
+export default function CGCatalog({cgJson}:{cgJson:cgJson}) {
+
     return (
         <>
-            Explore{cgJson.count} Campgrounds in our catalog
+            Explore {cgJson.count} Campgrounds in our catalog
             <div style={{margin:"20px", display:"flex", 
                 flexDirection:"row", alignContent:"space-around",
                 justifyContent:"space-around", flexWrap:"wrap", padding:"10px"}}>
                     {
-                        cgJson.data.map((cgItem:Object)=>(
+                        cgJson.data.map((cgItem:cgItem)=>(
                             <Link href={`/campground/${cgItem.id}`} className="w-1/5">
-                                <Card cgName={cgItem.model} imgSrc={cgItem.picture}/>
+                                <Card cgName={cgItem.name} imgSrc={cgItem.picture}/>
                             </Link>
                         ))
                     }
